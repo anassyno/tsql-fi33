1e7183f0403656b1eac3c121b7c88d34ee8b8a71
ASyn <asynodinos@cdtnue.onmicrosoft.com>
2026-06-25 10:01:53 +0200

    forgot to save

--- project1/SQLQuery5.sql
+++ project1/SQLQuery5.sql
@@ -1,3 +1,5 @@
+--Wiederholung
+
 select * from mein_test3;
 
 update mein_test4 set vorname = 'Lukas' where id = 1;
@@ -35,3 +37,44 @@ truncate table mein_test4;
 --sp_rename 'mein_test3', 'kermit';
 
 --sp_rename 'vornamen', 'mein_test3';
+
+
+--Tabelle aus Tabelle selektiv
+
+select * from kermit;
+
+select column_name, data_type, character_maximum_length
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'kermit';
+
+create table k_archive (
+nr int,
+vname varchar(10)
+);
+
+insert into k_archive
+select * from kermit;
+
+select * from k_archive;
+delete from k_archive;
+
+insert into k_archive
+select * from kermit
+where vorname <> 'kermit' and vorname != 'null' and vorname is not null;
+
+drop table k_archive2;
+
+create table k_archive2 (
+id int identity(1,1),
+vname varchar(10)
+);
+
+insert into k_archive2
+select vorname from kermit;
+
+select * from k_archive2;
+
+select * into k_archive3 from kermit;
+
+select * from k_archive3;
+
